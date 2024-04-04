274ba4eabdd3b7a89d3f30b885de7a1d8f3923a3
diazdaiz <40154072+diazdaiz@users.noreply.github.com>
2024-04-04 08:31:10 +0700

    Avoiding csv bug

--- Scripts/ChoosingJobManager.cs
+++ Scripts/ChoosingJobManager.cs
@@ -1,6 +1,8 @@
 using Godot;
+using Godot.NativeInterop;
 using System;
 using System.Collections.Generic;
+// using System.IO;
 using System.Linq;
 
 public partial class ChoosingJobManager : CanvasLayer {
@@ -41,18 +43,105 @@ public partial class ChoosingJobManager : CanvasLayer {
 
     public override void _Ready() {
         Jobs = new();
-        string classesInformationRaw = FileAccess.Open("res://Resources/class.csv", FileAccess.ModeFlags.Read).GetAsText();
-        List<string> classesInformationRows = classesInformationRaw.Split(new string[] { "\n" }, StringSplitOptions.None).ToList();
-        for (int i = 0; i < classesInformationRows.Count; i++) {
-            if (string.IsNullOrEmpty(classesInformationRows[i])) {
-                continue;
+        using (FileAccess classesInformationFileAccess = FileAccess.Open("res://Resources/class.txt", FileAccess.ModeFlags.Read)) {
+            string classesInformationRaw = classesInformationFileAccess.GetAsText();
+            List<string> classesInformationRows = classesInformationRaw.Split(new string[] { "\n" }, StringSplitOptions.None).ToList();
+            for (int i = 0; i < classesInformationRows.Count; i++) {
+                if (string.IsNullOrEmpty(classesInformationRows[i])) {
+                    continue;
+                }
+                if (i == 0) {
+                    continue;
+                }
+                string[] rowElements = classesInformationRows[i].Split(',');
+                Jobs.Add(new(rowElements[0].ToInt(), rowElements[1], rowElements[2], rowElements[3].Split(';').ToList()));
             }
-            if (i == 0) {
-                continue;
-            }
-            string[] rowElements = classesInformationRows[i].Split(',');
-            Jobs.Add(new(rowElements[0].ToInt(), rowElements[1], rowElements[2], rowElements[3].Split(';').ToList()));
         }
+
+        // using FileAccess classesInformationRaw = FileAccess.Open("res://Resources/class.csv", FileAccess.ModeFlags.Read);
+        // int n = -1;
+        // while (!classesInformationRaw.EofReached()) {
+        //     n += 1;
+        //     string[] rowElements = classesInformationRaw.GetCsvLine();
+        //     if (rowElements.Length == 0) {
+        //         continue;
+        //     }
+        //     if (string.IsNullOrEmpty(rowElements[0])) {
+        //         continue;
+        //     }
+        //     if (n == 0) {
+        //         continue;
+        //     }
+        //     // string temp = "";
+        //     // foreach (string rowElement in rowElements) {
+        //     //     temp += rowElement + ", ";
+        //     // }
+        //     // GD.Print(temp);
+        //     Jobs.Add(new(rowElements[0].ToInt(), rowElements[1], rowElements[2], rowElements[3].Split(';').ToList()));
+        // }
+
+        // using (var reader = new StreamReader("Resources/class.csv")) {
+        //     int n = -1;
+        //     while (!reader.EndOfStream) {
+        //         var line = reader.ReadLine();
+        //         n += 1;
+        //         string[] rowElements = line.Split(',');
+        //         if (rowElements.Length == 0) {
+        //             continue;
+        //         }
+        //         if (string.IsNullOrEmpty(rowElements[0])) {
+        //             continue;
+        //         }
+        //         if (n == 0) {
+        //             continue;
+        //         }
+        //         string temp = "";
+        //         foreach (string rowElement in rowElements) {
+        //             temp += rowElement + ", ";
+        //         }
+        //         GD.Print(temp);
+        //         Jobs.Add(new(rowElements[0].ToInt(), rowElements[1], rowElements[2], rowElements[3].Split(';').ToList()));
+        //     }
+        // }
+
+        // using (var reader = new StreamReader("Resources/class.txt")) {
+        //     int n = -1;
+        //     while (!reader.EndOfStream) {
+        //         var line = reader.ReadLine();
+        //         n += 1;
+        //         string[] rowElements = line.Split(',');
+        //         if (rowElements.Length == 0) {
+        //             continue;
+        //         }
+        //         if (string.IsNullOrEmpty(rowElements[0])) {
+        //             continue;
+        //         }
+        //         if (n == 0) {
+        //             continue;
+        //         }
+        //         string temp = "";
+        //         foreach (string rowElement in rowElements) {
+        //             temp += rowElement + ", ";
+        //         }
+        //         GD.Print(temp);
+        //         Jobs.Add(new(rowElements[0].ToInt(), rowElements[1], rowElements[2], rowElements[3].Split(';').ToList()));
+        //     }
+        // }
+
+        // FileAccess classesInformationFileAccess = FileAccess.Open("res://Resources/class.txt", FileAccess.ModeFlags.ReadWrite);
+        // string classesInformationRaw = classesInformationFileAccess.GetAsText();
+        // List<string> classesInformationRows = classesInformationRaw.Split(new string[] { "\n" }, StringSplitOptions.None).ToList();
+        // for (int i = 0; i < classesInformationRows.Count; i++) {
+        //     if (string.IsNullOrEmpty(classesInformationRows[i])) {
+        //         continue;
+        //     }
+        //     if (i == 0) {
+        //         continue;
+        //     }
+        //     string[] rowElements = classesInformationRows[i].Split(',');
+        //     Jobs.Add(new(rowElements[0].ToInt(), rowElements[1], rowElements[2], rowElements[3].Split(';').ToList()));
+        // }
+
         jobSelectionBackground = GetNode<ColorRect>("JobSelection");
         availableJobs = GetNode<OptionButton>("JobSelection/OptionButton");
         foreach (Job job in Jobs) {
@@ -65,6 +154,9 @@ public partial class ChoosingJobManager : CanvasLayer {
     }
 
     public override void _Process(double delta) {
+        if (Input.IsActionJustPressed("Exit")) {
+            GetTree().Quit();
+        }
         if (availableJobs.Selected != choosenJobIndex) {
             UpdateJobOverview();
             previousColor = jobSelectionBackground.Color;
